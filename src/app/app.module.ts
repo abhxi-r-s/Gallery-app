7579de314f2d168f29aa48b458557c507ddebc16
abhxi-r-s <abhishekunni8281@gmail.com>
2022-07-08 15:53:48 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewGalleryComponent } from './view-gallery/view-gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewGalleryComponent
   ],
   imports: [
     BrowserModule,
